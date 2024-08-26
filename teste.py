8c9c869b7f311f738dddb8fcacb644a25eef5a5b
Le.Costa <leticia.k.fontes@gmail.com>
2024-08-26 06:58:54 -0300

    Commit 2608_0658

--- teste.py
+++ teste.py
@@ -1,4 +1,4 @@
-""""
+'''
 numeros = list(range(1, 101))
 for numero in numeros:
     if numero % 2 == 0 and numero % 4 == 0:
@@ -6,10 +6,10 @@ for numero in numeros:
 
 div4 = [numero for numero in numeros if numero % 2 == 0 and numero % 4 == 0]
 print(div4)
-"""
+
 
 ### ("CALCULO DE AREA DE PARALEOGRAMA###
-'''
+
 print("Calculador de area de paralelogrma")
 
 base = float(input("Insira o comprimento a base:"))
@@ -41,7 +41,7 @@ elif ope == "/":
     print("O resultado é: ", resultado)
 else:
     print("Operação inválida!")
-'''
+
 # CONVERSÃO DE NUMEROS#
 
 float(9)
@@ -64,3 +64,72 @@ round(3.1548657, 4)
 # Potencia
 
 pow(5, 3)
+
+
+## variaveis##
+
+# atribuindo valor
+
+var1 = 3
+# impressao
+print(var1)
+
+# declaração multipla
+
+pessoa1, pessoa2, pessoa3 = "Le", "Wagner", "Nadia"
+
+
+print(pessoa1)
+print(pessoa2)
+print(pessoa3)
+
+fruta1 = fruta2 = fruta3 = "Pessego"
+
+print(fruta2)
+
+
+# CONCATENAÇÃO DE VARIAVEL#
+
+TextoAd = input("Insira um texto: ")
+
+nome = "Leticia"
+sobrenome = "Costa"
+
+FullName = nome + " " + sobrenome + " " + TextoAd
+
+print(FullName)
+'''
+
+# STRING E INDEXAÇÃO#
+
+# Pode se usar aspas simples ou duplas
+
+# QUEBRA DE LINHA#
+
+print('teste \n quebra \n de \n linha')
+
+# indexação de string#
+
+# atribuindo valor
+
+x = 'Primeira string'
+
+print(x)
+
+# indexação começa com 0
+
+print(x[0])
+
+print(x[5])
+
+# UM SLICING COMEÇA PELA POSIÇÃO X ATÉ O FIM DA STR
+#: no fim, retorna tudo até  o fim da str
+print(x[3:])
+
+#: no começo retorna tudo até indice x
+
+print(x[:7])
+
+# indexação negativa lede tras pra frente
+
+print(x[:-1])
