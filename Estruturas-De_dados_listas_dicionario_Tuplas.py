bfa39919501f90ca152530f61220769ed837e4e5
Le.Costa <leticia.k.fontes@gmail.com>
2024-09-04 22:25:44 -0300

    09092024_2225_testes

--- Estruturas-De_dados_listas_dicionario_Tuplas.py
+++ Estruturas-De_dados_listas_dicionario_Tuplas.py
@@ -1,6 +1,6 @@
 # criando listas
 # Listas ficam entre colchetes, tuplas entre parenteses
-
+'''
 lista_1 = ['arroz,feijão,macarrão']  # lista com UMA string
 
 print(lista_1)
@@ -38,9 +38,59 @@ print(lista_2)
 print(lista_1)
 
 ###### LISTA DE LISTAS#######
+# As lista de listas são as matrizes
 
 lista_a = [[1, 2, 3], [56, 78, 99], [87, 89, 54]]
 print(lista_a)
 
 lista_b = [['aaa', 'bbbbb'], ['hkkhdhd', 'okjg']]
 print(lista_b)
+
+# Atribuição de um item da lista a uma variavel
+
+a = lista_a[0]  # a recebeu a lista que esta no indice 0 (1,2,3)
+b = a[0]  # b recebeu o indice 0 de a (1)
+
+valor1 = lista_b[1]
+print(valor1[0])
+
+salada = [['tomate', 'abacate'],
+         [ 'rucula', 'alface', 'agriao'],
+        ['cenoura', 'brocolis']]
+frutas = salada[0]
+folhas = salada[1]
+vegetais = salada[2]
+
+print(frutas[1])
+print(folhas[2])
+print(vegetais[0])
+
+
+## OPERAÇÃO COM LISTAS###
+
+lista_num = [[0, 1, 2], [20, 25, 30], [22, 24, 26, 28]]
+print(lista_num)
+
+a = lista_num[1][0]  # 20
+b = lista_num[2][3]  # 28
+print(a, b)
+
+soma = lista_num[2][0] + 10  # 32
+
+print(soma)
+
+d = 10
+e = d * lista_num[2][2]
+print(e)
+'''
+## CONCATENAÇÃO DE LISTAS###
+frutas = [['amora', 'tomate', 'uva'], ['maça', 'pera', 'ameixa']]
+vegetais = [['alface', 'rucula']]
+
+salada = frutas + vegetais
+
+print(salada)  # Não concatena como uma lista só
+
+# Para sair uma unica lista
+vegetais = [item for sublista in (frutas + vegetais)for item in sublista]
+print(vegetais)
