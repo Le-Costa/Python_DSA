612f9a4634d5c82622b8857506ec6bbf9a11f35b
Le-Costa <leticia.k.f.dacosta@gmail.com>
2024-09-17 17:01:31 -0300

    Update estruturas de repetição.py

--- estruturas de repetição.py
+++ estruturas de repetição.py
@@ -1,5 +1,5 @@
 ##Condicionais####
-
+'''
 ##condicional IF##
     #se algo acontecer faça algo
 if 5>2:
@@ -27,3 +27,43 @@ elif dia == 'terça':
     print ("hoje vai chover")
 else: 
     print ('Sem previsão para essa data')
+
+#operadores lógicos#
+
+- Operador **and** - Retorna True se ambas as declarações forem verdadeiras.
+- Operador **or** - Retorna True se uma das declarações for verdadeira.
+- Operador **not** - Inverte o resultado, retorna False se o resultado for True.
+
+
+#AND
+numero = 4 
+if (numero > 2 ) and (numero %2 ==0):
+    print ("Duas condições verdadeiras ")
+else: 
+    print ("Uma das condições é falsa ") 
+
+#OR
+numero = 4 
+if (numero > 5 ) or(numero %2 == 0 ):
+    print ("Uma das condições é verdadeira ")
+
+#NOT 
+if not (numero >5) and (numero %2 ==0):
+    print ("As duas cndições são verdadeiras")
+else: 
+    print ("Uma das duas condições são falsas ")
+
+    ##AND OR E NOT##
+if (not (numero >5 )and (numero %2 == 0)) or (numero ==4):
+  print ("As duas primeiras condições são verdadeiras ou a terceira é verdadeira. ")  
+
+  ##INTRODUÇÃO DE PLACEHOLDERS##
+disciplina = 'Data science'
+nota_final = 90
+semestre = 2
+
+if disciplina == 'Data science' and nota_final >=80 and semestre != 1: 
+    print ("Aprovado em %s com média final %r!" %(disciplina,nota_final))
+else: 
+    print ("Reprovado") '''
+
