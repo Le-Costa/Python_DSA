f6fcc6c09f9b4d061388953bf35b18b3c1c69672
Le.Costa <leticia.k.fontes@gmail.com>
2024-09-23 19:33:46 -0300

    23092024_1933

--- Metodos_Intervalos_funcoes.py
+++ Metodos_Intervalos_funcoes.py
@@ -1,73 +1,105 @@
-#Intervalo de valores com a Função Range## 
- 
-#loop para percorrer numeros de 1 a 10
-for i in range (1,11): #o ultimo número é exclusivo, não é impresso
-    print (i)
+# Intervalo de valores com a Função Range##
 
-#definição de saltos 
-for i in range (50,101,20): #primeiro numero da coleção, segundo numero, salto de numeros
-    print (i) 
+# loop para percorrer numeros de 1 a 10
+for i in range(1, 11):  # o ultimo número é exclusivo, não é impresso
+    print(i)
 
-for i in range (0,-20,-5): #pode ser usada com valores negativos 
-    print (i)
+# definição de saltos
+for i in range(50, 101, 20):  # primeiro numero da coleção, segundo numero, salto de numeros
+    print(i)
 
-#usando o tamanho de uma lista na função range
-lista = ['abacaxi','banana','morango','uva']
-lista_tamanho = len (lista ) #atribuição da função len (tamanho) a uma variavel
-for i in range (0,lista_tamanho):
-    print (lista [i])
+for i in range(0, -20, -5):  # pode ser usada com valores negativos
+    print(i)
 
-                                ##Métodos##
-#tudo em python é objeto. Cada objeto tem métodos e atributos
-#métodos são funções com ações que podem ser executadas nos objetos
-#os objetos tem vários métodos a serem usados
+# usando o tamanho de uma lista na função range
+lista = ['abacaxi', 'banana', 'morango', 'uva']
+lista_tamanho = len(lista)  # atribuição da função len (tamanho) a uma variavel
+for i in range(0, lista_tamanho):
+    print(lista[i])
 
-lista = [1,2,3,4,100]
+    ## Métodos##
+# tudo em python é objeto. Cada objeto tem métodos e atributos
+# métodos são funções com ações que podem ser executadas nos objetos
+# os objetos tem vários métodos a serem usados
 
-print (lista.append(100)) #adiciona elementos
-print (lista.count(100)) #conta os elementos passados
-print (dir (lista)) #mostra os atributos do objeto
+lista = [1, 2, 3, 4, 100]
+
+print(lista.append(100))  # adiciona elementos
+print(lista.count(100))  # conta os elementos passados
+print(dir(lista))  # mostra os atributos do objeto
+
+## Construção de funções##
+# bloco de código que pode executar várias vezes fazendo a chamada
+# quando a função faz parte de uma classe, ela é chamada de método
+
+# definindo a função
 
 
-                    ##Construção de funções##
-#bloco de código que pode executar várias vezes fazendo a chamada
-#quando a função faz parte de uma classe, ela é chamada de método
+def primeira_func():
+    nome = "bob"
+    print('hello %s' % (nome))
 
-#definindo a função
-def primeira_func ():
- nome = "bob"
- print ('hello %s' %(nome))
 
-print (primeira_func())
- 
-#definindo função com parametro
-def segundafunc (nome): #{1 parametro
- print ("Hello %s"% (nome))
+print(primeira_func())
 
-#para chamar a função tem que passar o parametro
+# definindo função com parametro
+
+
+def segundafunc(nome):  # {1 parametro
+    print("Hello %s" % (nome))
+
+
+# para chamar a função tem que passar o parametro
 segundafunc('Leticia')
 
-#função para imprimir numeros
-def imprime_numeros (): #{ nenhum parametro
- for i in range (0,5):
-  print ('número: ' + str(i))
+# função para imprimir numeros
+
+
+def imprime_numeros():  # { nenhum parametro
+    for i in range(0, 5):
+        print('número: ' + str(i))
+
 
 imprime_numeros()
 
-#Função para soma de numeros
-def addnum (firstnum,secnum): #{dois parametros
- print ("Primeiro número: " + str(firstnum)) #str converte o nuemro p/ string para que ele possa ser concatenado com a string
- print ("Segundo número: " + str(secnum))
- print ("soma: ", firstnum + secnum)
+# Função para soma de numeros
 
-addnum (45,45)
 
-#Funções com nuemro variavel de parametros 
-def print_var_info (arg1,*vartuple): 
-    print ('parametro passado: ',arg1)
+def addnum(firstnum, secnum):  # {dois parametros
+    # str converte o nuemro p/ string para que ele possa ser concatenado com a string
+    print("Primeiro número: " + str(firstnum))
+    print("Segundo número: " + str(secnum))
+    print("soma: ", firstnum + secnum)
+
+
+addnum(45, 45)
+
+# Funções com nuemro variavel de parametros
+
+
+def print_var_info(arg1, *vartuple):
+    print('parametro passado: ', arg1)
     for item in vartuple:
-        print ('parametro passado: ',item)
-    return;
+        print('parametro passado: ', item)
+    return
 
-print_var_info (10)
-print_var_info (100,20,'morango') 
+
+print_var_info(10)
+print_var_info(100, 20, 'morango')
+
+# escopo de variavel local e global
+# variaveis globais estão fora de funções e podemser acessadas em qualquer parte do código
+# variaveis locais só existem dentro de uma função e só podem ser acessadas por tal
+
+# revisão Funções bult in
+# são funções prontas
+print(abs(56))  # retorna o valor absoluto
+print(bool(10))  # valor boleano
+print(int(4.325678))  # retorna o valor inteiro
+print(str(13))  # converte numero para string
+print(float(5))  # converte para decimal
+print(len([1, 2, 3, 4, 5, 6]))  # retorna tamanho
+array = [1, 2, 3, 4]
+print(max(array))  # maior numero da variavel
+print(min(array))  # menor numero da variavel
+print("soma: ", sum(array))
--- estruturas_de _repetição.py
+++ estruturas_de _repetição.py
@@ -1,191 +1,189 @@
-##Condicionais####
+## Condicionais####
 
-##condicional IF##
-    #se algo acontecer faça algo
-if 5>2:
-    print ("Verdadeiro!") #{ nesse caso se não for verdadeiro, não acontecerá nada, por isso o uso do else:}
+## condicional IF##
+# se algo acontecer faça algo
+if 5 > 2:
+    # { nesse caso se não for verdadeiro, não acontecerá nada, por isso o uso do else:}
+    print("Verdadeiro!")
 
 ## if com else ###
 
-if 5<2 :
-    print ("verdadeiro")
+if 5 < 2:
+    print("verdadeiro")
 else:
-    print ("falso")
+    print("falso")
 
-#inclusão de variaveis 
+# inclusão de variaveis
 dia = 'terça'
 if dia == 'segunda':
-    print ("hoje fará sol!")
-else: 
-    print ("hoje irá chover")
+    print("hoje fará sol!")
+else:
+    print("hoje irá chover")
 
-#ELIF valida mais de uma condição 
+# ELIF valida mais de uma condição
 
 if dia == 'segunda':
-    print ('hoje fará sol')
+    print('hoje fará sol')
 elif dia == 'terça':
-    print ("hoje vai chover")
-else: 
-    print ('Sem previsão para essa data')
+    print("hoje vai chover")
+else:
+    print('Sem previsão para essa data')
 
-#operadores lógicos#
-'''
-- Operador **and** - Retorna True se ambas as declarações forem verdadeiras.
-- Operador **or** - Retorna True se uma das declarações for verdadeira.
-- Operador **not** - Inverte o resultado, retorna False se o resultado for True.
-'''
+# operadores lógicos#
 
-#AND
-numero = 4 
-if (numero > 2 ) and (numero %2 ==0):
-    print ("Duas condições verdadeiras ")
-else: 
-    print ("Uma das condições é falsa ") 
+- Operador ** and ** - Retorna True se ambas as declarações forem verdadeiras.
+- Operador ** or ** - Retorna True se uma das declarações for verdadeira.
+- Operador ** not ** - Inverte o resultado, retorna False se o resultado for True.
 
-#OR
-numero = 4 
-if (numero > 5 ) or(numero %2 == 0 ):
-    print ("Uma das condições é verdadeira ")
 
-#NOT 
-if not (numero >5) and (numero %2 ==0):
-    print ("As duas cndições são verdadeiras")
-else: 
-    print ("Uma das duas condições são falsas ")
+# AND
+numero = 4
+if (numero > 2) and (numero % 2 == 0):
+    print("Duas condições verdadeiras ")
+else:
+    print("Uma das condições é falsa ")
 
-    ##AND OR E NOT##
-if (not (numero >5 )and (numero %2 == 0)) or (numero ==4):
-  print ("As duas primeiras condições são verdadeiras ou a terceira é verdadeira. ")  
+# OR
+numero = 4
+if (numero > 5) or (numero % 2 == 0):
+    print("Uma das condições é verdadeira ")
 
-  ##INTRODUÇÃO DE PLACEHOLDERS##
+# NOT
+if not (numero > 5) and (numero % 2 == 0):
+    print("As duas cndições são verdadeiras")
+else:
+    print("Uma das duas condições são falsas ")
+
+    ## AND OR E NOT##
+if (not (numero > 5) and (numero % 2 == 0)) or (numero == 4):
+    print("As duas primeiras condições são verdadeiras ou a terceira é verdadeira. ")
+
+    ## INTRODUÇÃO DE PLACEHOLDERS##
 disciplina = 'Data science'
 nota_final = 90
 semestre = 2
 
-if disciplina == 'Data science' and nota_final >=80 and semestre != 1: 
-    print ("Aprovado em %s com média final %r!" %(disciplina,nota_final))
-else: 
-    print ("Reprovado") 
+if disciplina == 'Data science' and nota_final >= 80 and semestre != 1:
+    print("Aprovado em %s com média final %r!" % (disciplina, nota_final))
+else:
+    print("Reprovado")
 
 ##### LOOP FOR ####
-tp =(2,3,4)
-for i in tp: 
-    print (i)
+tp = (2, 3, 4)
+for i in tp:
+    print(i)
 
-frutas = ['amora','tomate','uva']
-for i in frutas :
-    print (i) 
+frutas = ['amora', 'tomate', 'uva']
+for i in frutas:
+    print(i)
 
-for contador in range (0,5): #conjunção de elementos#
-    print (contador)
+for contador in range(0, 5):  # conjunção de elementos#
+    print(contador)
 
-#impressaõ de numero pares em uma lista# #Loop com condição#
+# impressaõ de numero pares em uma lista# #Loop com condição#
 
-numeros =[1,2,3,4,5,6,7,8,9,10]
-print ('numeros pares: ')
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print('numeros pares: ')
 for num in numeros:
-    if num %2 == 0:
-        print (num)
+    if num % 2 == 0:
+        print(num)
 
-for  i in range (0,101,2): #range com incrementeo: (primeiro numero, segundo numero, incremento).
+# range com incrementeo: (primeiro numero, segundo numero, incremento).
+for i in range(0, 101, 2):
     # Nesse caso vai percorrer de 0 a 101 pulando de dois em dois
-    print (i)
+    print(i)
 
 for caracter in "Estou usando o vscode ":
-    print (caracter)
+    print(caracter)
 
-  
 
-#LOOPS ANINHADOS#
-  
-#loop aninhado
-lista1 = [0,1,2,3,4]
-lista2 = [1,2,3,]
+# LOOPS ANINHADOS#
 
-#loop externo 
- 
-for elemento_lista1 in lista1 :
+# loop aninhado
+lista1 = [0, 1, 2, 3, 4]
+lista2 = [1, 2, 3,]
+
+# loop externo
+
+for elemento_lista1 in lista1:
     for elemento_lista2 in lista2:
-        print ('\n', elemento_lista1 * elemento_lista2)
-    print ('-----')   
+        print('\n', elemento_lista1 * elemento_lista2)
+    print('-----')
 
 
-#o numero 47 aparece nas duas listas?  
-lista3 = [10,16,24,39,47]
-lista4 = [32,89,47,76,12]
-  
+# o numero 47 aparece nas duas listas?
+lista3 = [10, 16, 24, 39, 47]
+lista4 = [32, 89, 47, 76, 12]
+
 for elemento_lista3 in lista3:
-        for elemento_lista4 in lista4:
-             if elemento_lista3 == 47 and elemento_lista4 == 47:
-                  
-                  print ("O número 47 aparece nas duas listas")
+    for elemento_lista4 in lista4:
+        if elemento_lista3 == 47 and elemento_lista4 == 47:
+
+            print("O número 47 aparece nas duas listas")
 
 
+# soma dos numros pares da lisat3 com os da lista4
+soma = 0
 
-#soma dos numros pares da lisat3 com os da lista4
-soma=0
+for lista in [lista3, lista4]:
 
-for lista in [lista3,lista4]:
-     
-     for num in lista: 
-          
-          if num %2 ==0:
-               soma += num # sinal de atribuição +=#
-print ("A soma dos números pares é = a:", soma) 
+    for num in lista:
 
-  
+        if num % 2 == 0:
+            soma += num  # sinal de atribuição +=#
+print("A soma dos números pares é = a:", soma)
 
-#Aquele é diferente deste #
-soma=0
+
+# Aquele é diferente deste #
+soma = 0
 for num in lista3 + lista4:
-    if num %2 ==0:
-            soma += num # sinal de atribuição +=#
-print ("A soma dos números pares é = a:", soma) 
+    if num % 2 == 0:
+        soma += num  # sinal de atribuição +=#
+print("A soma dos números pares é = a:", soma)
 
 
-matriz = [[42,23,34],[100,215,114],[10.1,98.7,12.3]]
+matriz = [[42, 23, 34], [100, 215, 114], [10.1, 98.7, 12.3]]
 maior_numero = 0
 
 for linha in matriz:
     for num in linha:
-         if num > maior_numero:
-            maior_numero = num 
-print ("maior nuemro: ", maior_numero)
+        if num > maior_numero:
+            maior_numero = num
+print("maior nuemro: ", maior_numero)
 
 
-##LOOP WHILE##
+## LOOP WHILE##
 
-#a condição tem que ser verdadeira pelo menos uma vez
-#Ela tem que parar de ser verdadeira em algum momento, se não da um loop infinito 
+# a condição tem que ser verdadeira pelo menos uma vez
+# Ela tem que parar de ser verdadeira em algum momento, se não da um loop infinito
 
-valor =0
-while valor <5:
-    print (valor)
-    valor = valor + 1 
+valor = 0
+while valor < 5:
+    print(valor)
+    valor = valor + 1
 
-#Pode se usar else com while
-x=0 
-while x <5:
-    print ("X nesta iteração :", x)
-    print ("x ainda é menor que 10, somando 1 a x")
-    x+=1 
+# Pode se usar else com while
+x = 0
+while x < 5:
+    print("X nesta iteração :", x)
+    print("x ainda é menor que 10, somando 1 a x")
+    x += 1
 else:
-    print ("loop concluido")
-print (x)
+    print("loop concluido")
+print(x)
 
-#PASS, BREAKE E CONTINUE# 
+# PASS, BREAKE E CONTINUE#
 
 var1 = 0
-while var1 < 5:       #primeiro loop acontecerá enquanto a var1 for menor que 5 
-    if var1 ==4 :  #depois de checar se é menor que 5 verifica se é igual 4 
-        break       # se for igual a 4 o brek para o loop       
-    else:           # se não for igual a 4 passa para o comando seguinte  
+while var1 < 5:  # primeiro loop acontecerá enquanto a var1 for menor que 5
+    if var1 == 4:  # depois de checar se é menor que 5 verifica se é igual 4
+        break       # se for igual a 4 o brek para o loop
+    else:           # se não for igual a 4 passa para o comando seguinte
         pass
-    print (var1)
-    var1 = var1 +1     
+    print(var1)
+    var1 = var1 + 1
 
 for letra in "Estou usando o zzz vscode":
-    if letra == 'z': #Condição para continuar o código e ignorar
-        continue  #Desconsidera o a letra#
-    print (letra)
-
+    if letra == 'z':  # Condição para continuar o código e ignorar
+        continue  # Desconsidera o a letra#
+    print(letra)
